2d9809d1876536c63e9bed498df7e78df9f03b9f
foxofarctic <lae1@williams.edu>
2018-07-06 17:44:17 -0400

    added some fossil things

--- shale.py
+++ shale.py
@@ -1,21 +1,35 @@
 # csv library
 import csv
 
+# filenames of data
 periods = ['SilurianColumns.csv', 'DevonianColumns.csv', 'CarboniferousColumns.csv']
+
 for period in periods:
     
     with open(period, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-        # total area of devonian rock found
+        # total area of exposed rock found in period
         totalArea = 0
         # total area of units including shale
-        shaleArea= 0
+        shaleArea = 0
+        # fossiliferous areas
+        fossilArea = 0
+        
         for row in reader:
+            localArea = float(row['col_area'])
+            if int(row['pbdb_collections']) > 0:
+                fossilArea += localArea
+                
             if "shale" in str(row['lith']):
-                shaleArea += float(row['col_area'])
-            totalArea += float(row['col_area'])
-        print( "For CSV File: " + period)
+                shaleArea += localArea
+            totalArea += localArea
+
+        # Calculate Percent Shale
+        percentShale = (shaleArea/totalArea) * 100.0
+
+        # Print Data
+        print( "For CSV File: " + period)    
         print("Total Area: " + str(totalArea))
         print("Shale Area: " + str(shaleArea))
-        print( "Exposed Percent Shale: " + str((shaleArea/totalArea) * 100))
-
+        print("Fossiliferous Area: " + str(fossilArea))
+        print( "Exposed Percent Shale: " + str(percentShale) + "\n" )
