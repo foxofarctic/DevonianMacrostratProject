a6a09a505acb89c2de9906f8fae850dd709fa0ae
foxofarctic <lae1@williams.edu>
2018-07-06 16:25:39 -0400

    added in Silurian and carbonif datasets

--- shale.py
+++ shale.py
@@ -1,16 +1,21 @@
 # csv library
 import csv
-with open('DevonianColumns.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    # total area of devonian rock found
-    totalArea = 0
-    # total area of units including shale
-    shaleArea= 0
-    for row in reader:
-        if "hale" in str(row['lith']):
-            shaleArea += float(row['col_area'])
-        totalArea += float(row['col_area'])
-    print("Total Area: " + str(totalArea))
-    print("Shale Area: " + str(shaleArea))
-    print( "Exposed Devonian Percent Shale: " + str((shaleArea/totalArea) * 100))
+
+periods = ['SilurianColumns.csv', 'DevonianColumns.csv', 'CarboniferousColumns.csv']
+for period in periods:
+    
+    with open(period, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        # total area of devonian rock found
+        totalArea = 0
+        # total area of units including shale
+        shaleArea= 0
+        for row in reader:
+            if "shale" in str(row['lith']):
+                shaleArea += float(row['col_area'])
+            totalArea += float(row['col_area'])
+        print( "For CSV File: " + period)
+        print("Total Area: " + str(totalArea))
+        print("Shale Area: " + str(shaleArea))
+        print( "Exposed Percent Shale: " + str((shaleArea/totalArea) * 100))
 
