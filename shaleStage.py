ff0dc73da73579f63c46896f5ac0c9eff8607e20
foxofarctic <lae1@williams.edu>
2018-07-11 16:40:36 -0400

    added mudstone comparisons and second plot

--- shaleStage.py
+++ shaleStage.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 periods = ['DevonianColumns.csv']
 
 # stage data [b_age,t_age,total area, total shale area, total fossil area]
-fammenianT = [372.2,358.9,0,0,0,'Fammenian',0]
-frasnianT = [382.7,372.2,0,0,0,'Frasnian',0]
-givetianT = [387.7,382.7,0,0,0,'Givetian',0]
-eifelianT = [393.3,387.7,0,0,0,'Eifelian',0]
-emsianT = [407.6,393.3,0,0,0,'Emsian',0]
-pragianT = [410.8,407.6,0,0,0,'Pragian',0]
-lochkovianT = [419.2,410.8,0,0,0,'Lochkovian',0]
+fammenianT = [372.2,358.9,0,0,0,'Fammenian',0,0]
+frasnianT = [382.7,372.2,0,0,0,'Frasnian',0,0]
+givetianT = [387.7,382.7,0,0,0,'Givetian',0,0]
+eifelianT = [393.3,387.7,0,0,0,'Eifelian',0,0]
+emsianT = [407.6,393.3,0,0,0,'Emsian',0,0]
+pragianT = [410.8,407.6,0,0,0,'Pragian',0,0]
+lochkovianT = [419.2,410.8,0,0,0,'Lochkovian',0,0]
 
 # constants for stage data indices
 B_AGE = 0
@@ -26,6 +26,7 @@ SHALE_AREA = 3
 FOSSIL_AREA = 4
 STAGE_NAME = 5
 TOTAL_LOCALITIES = 6
+MUDSTONE_AREA = 7
 
 #constants for rockType
 ROCK = 0
@@ -76,20 +77,28 @@ for period in periods:
                         if int(row['pbdb_collections']) > 0:
                             stage[FOSSIL_AREA] += localArea
 
-                        # shale present and rock thickness - thickness set to 0.80
+                        # check for rock Types
                         for rock in rockTypes:
-                            
+
+                            # check shale, thickness > .2
                             if ('shale siliciclastic' in rock[ROCK]
-                                and rock[THICKNESS] > 0.80):
+                                and rock[THICKNESS] > 0.2):
                                 stage[SHALE_AREA] += localArea
 
+                            # check mudstone, thickness > 0.0
+                            if ('mudstone siliciclastic' in  rock[ROCK]
+                                and rock[THICKNESS] > 0.0):
+                                stage[MUDSTONE_AREA] += localArea
+                                
                         # add area and locality
                         stage[TOTAL_AREA] += localArea
                         stage[TOTAL_LOCALITIES] += 1
 
+    # Arrays of various rock type Areas
     shaleAreaArr = []
     stageB_ageArr = []
-
+    mudstoneAreaArr = []
+    
     # loop through various stages for data printing
     for stage in stageTimes:
         
@@ -108,20 +117,30 @@ for period in periods:
         if stage[TOTAL_AREA] != 0:
             percentShale = (stage[SHALE_AREA]/stage[TOTAL_AREA]) * 100.0
             percentFossil = (stage[FOSSIL_AREA]/stage[TOTAL_AREA]) * 100.0
-
+            percentMudstone = (stage[MUDSTONE_AREA]/stage[TOTAL_AREA]) * 100.0
+            
             # arrays for scatter plot
             shaleAreaArr.append(percentShale)
             stageB_ageArr.append(stage[STAGE_NAME])
-
+            mudstoneAreaArr.append(percentMudstone)
+            
             print( "Exposed Percent Fossiliferous Area: " + str(percentFossil))
             print( "Exposed Percent Shale: " + str(percentShale))
+            print( "Exposed Percent Mudstone: " + str(percentMudstone))
         print("-------------------------------------------------------")
 
-# plot of data
+# plot of shale data
+plt.figure(1)
 plt.plot(stageB_ageArr, shaleAreaArr, 'o-')
 plt.ylabel('Percent Shale')
 plt.xlabel('Stage')
 plt.title('Percent Shale through Devonian')
+
+# plot of mudstone data
+plt.figure(2)
+plt.plot(stageB_ageArr, mudstoneAreaArr, 'o-')
+plt.title('Percent Mudstone through Devonian')
+
 plt.show()
 
 
