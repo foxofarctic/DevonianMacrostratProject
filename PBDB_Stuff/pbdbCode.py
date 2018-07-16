34cb8057d36b25f57f88303cf25666d39ba08600
foxofarctic <lae1@williams.edu>
2018-07-16 16:44:41 -0400

    finally figured out how to process json data

--- PBDB_Stuff/pbdbCode.py
+++ PBDB_Stuff/pbdbCode.py
@@ -1,11 +1,20 @@
 #import urllib.request, json
 import requests, json
+
+# API link for data from pbdb in JSON format
 url = 'https://paleobiodb.org/data1.2/occs/list.json?datainfo&rowcount&interval=famennian,famennian&show=genus,timebins'
-#response = urllib.request.urlopen(url)
-#str_response = response.read().decode('utf-8')
-#data = json.loads(str_response)
-response = requests.get(url)
-todos= json.loads(response.text)
-print(type(todos))
-#print(data)
-#todos[:10]
+
+url2 = 'http://paleobiodb.org/data1.2/taxa/single.json?name=Dascillidae&show=attr'
+
+# receive data from api
+response = requests.get(url2)
+
+# create dict object with data response
+data = response.json()
+
+# get list of records dicts
+records = data['records']
+
+print(recordsDict)
+print(type(recordsDict))
+
